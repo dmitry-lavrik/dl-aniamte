e9ca8353818e1cb81e82ca03cd0eff51db100a5c
legale <legale.legale@gmail.com>
2018-01-24 00:06:13 +0100

    major v0.2

--- src/dl-animate-class.js
+++ src/dl-animate-class.js
@@ -1,250 +1,275 @@
-! function(){
-class DLAnimate{
-	constructor(){
-		let raf = (function () {
-			let clock = Date.now();
-			return function (callback) {
-				let currentTime = Date.now();
+!function () {
+    class DLAnimate {
+        constructor() {
+            //create raf function
+            let raf = window.requestAnimationFrame ||
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function (callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+            //bind context window
+            this.raf = raf.bind(window);
 
-				if (currentTime - clock > 16) {
-					clock = currentTime;
-					callback(currentTime);
-				} else {
-					setTimeout(function () {
-						raf(callback);
-					}, 0);
+            let divTest = document.createElement("div");
+
+            /* checking needed features */
+            this.canAnimate = (typeof this.raf === "function") &&
+                ("classList" in divTest) &&
+                typeof divTest.style.transition !== undefined;
+
+            /* requestAnimationFrame queue */
+            this.frames = [];
+            //storage for _finisherHandler() method
+            this.finisher_show = false;
+            this.finisher_hide = false;
+
+            //storage for eventlistener functions
+            this.handler = {};
+
+            //animation in progress indicator
+            this.in_progress = false;
+        }
+
+        insert(target, el, options = {}, before = null) {
+            this._hide(el);
+            target.insertBefore(el, before);
+            this.show(el, options);
+        }
+
+        remove(el, options = {}) {
+            options.systemDoneCallback = function () {
+                el.parentNode.removeChild(el);
+            }
+
+            this.hide(el, options);
+        }
+
+        _reset() {
+            if (this.frames.length > 0) {
+                this.frames = [];
+            }
+            this._removeClasses(this.el, Object.values(this.settings.classNames));
+
+            if (this.finisher_show) {
+                this._finishHandler(this.finisher_show, true);
+            }
+            if (this.finisher_hide) {
+                this._finishHandler(this.finisher_hide, true);
+            }
+        }
+
+        show(el, options = {}) {
+            this.el = el;
+            //skip animate if requestAnimationFrame is not supported
+            if (!this.canAnimate) {
+                return this._show(el);
+            }
+
+            /* merge defaults and users options */
+            let settings = this._calcOptions(options);
+            this.settings = settings;
+
+            //reset to initial state
+            this._reset();
+
+            // return if element is NOT hidden
+            if (!this._isHidden(el) && !this.in_progress) {
+                return;
+            }
+
+            /* set handler on animation finish */
+            this.finisher_show = [el, settings.track, settings.duration, () => {
+                this._removeClasses(el, settings.classNames.enterActive);
+                this.finisher_show = false;
+                this.in_progress = false;
+                settings.systemDoneCallback(el);
+                settings.after(el);
+            }
+            ];
+
+            this._addFrame(() => {
+                this.in_progress = true;
+                this._show(el);
+                this._finishHandler(this.finisher_show);
+                this._addClasses(el, [settings.classNames.enterActive, settings.classNames.enter]);
+                settings.before(el);
+            });
+
+            this._addFrame(() => {
+                this._removeClasses(el, settings.classNames.enter);
+            });
+
+            this._nextFrame();
+        }
+
+        hide(el, options = {}) {
+            this.el = el;
+            if (!this.canAnimate) {
+                return this._hide(el);
+            }
+
+            let settings = this._calcOptions(options);
+            this.settings = settings;
+
+            this._reset();
+            //return if element is hidden and animation not in progress
+            if (this._isHidden(el) && !this.in_progress) {
+                return;
+            }
+
+            this.finisher_hide = [el, settings.track, settings.duration, () => {
+                this._hide(el);
+                this._removeClasses(el, settings.classNames.leaveActive);
+                options.systemOnEnd && options.systemOnEnd();
+                this.finisher_hide = false;
+                this.in_progress = false;
+                settings.systemDoneCallback(el);
+                settings.after(el);
+            }
+            ];
+
+
+            this._addFrame(() => {
+                this.in_progress = true;
+                this._finishHandler(this.finisher_hide);
+                this._addClasses(el, settings.classNames.leaveActive);
+                settings.before(el);
+            });
+
+            this._addFrame(() => {
+                this._addClasses(el, settings.classNames.leave);
+            });
+
+            this._nextFrame();
+        }
+
+        _finishHandler(arr, abort) {
+            let el = arr[0], track = arr[1], duration = arr[2], fn = arr[3], eventName, isCssTrack = true;
+
+            if (track === 'transition') {
+                eventName = 'transitionend';
+            } else if (track === 'animation') {
+                eventName = 'animationend';
+            } else {
+                isCssTrack = false;
+            }
+
+            if (abort) {
+                el.removeEventListener(eventName, this.handler[eventName]);
+                return;
+            }
+
+            this.handler[eventName] = () => {
+                el.removeEventListener(eventName, this.handler[eventName]);
+                fn();
+            };
+
+            if (isCssTrack) {
+                el.addEventListener(eventName, this.handler[eventName]);
+            } else {
+                setTimeout(fn, duration);
+            }
+        }
+
+        _calcOptions(options) {
+            let classNames = this._mergeSettings(this._classNames(options.name), options.classNames);
+            delete options.classNames;
+
+            let defaults = {
+                name: '',
+                track: 'transition',
+                duration: null,
+                classNames: classNames,
+                before(el){
+                },
+                after(el){
+                },
+                systemDoneCallback(el){
+                }
+            }
+            return this._mergeSettings(defaults, options);
+        }
+
+        _classNames(name) {
+            return !name ? {
+				enter: '',
+				enterActive: '',
+				leave: '',
+				leaveActive: '' 
+			} 
+            : {
+                enter: name + '-enter',
+                enterActive: name + '-enter-active',
+                leave: name + '-leave',
+                leaveActive: name + '-leave-active',
+            }
+        }
+
+        _addFrame(fn) {
+            this.frames.push(fn);
+        }
+
+        _nextFrame() {
+            if (this.frames.length === 0) {
+                return;
+            }
+
+            let frame = this.frames.shift();
+
+            frame();
+            //we use double raf because of a bug https://bugs.chromium.org/p/chromium/issues/detail?id=675795
+            this.raf(() => {
+                this.raf(() => {
+                    this._nextFrame();
+                });
+            });
+
+        }
+
+        _addClasses(el, str) {
+            this._classList('add', el, str);
+        }
+
+        _removeClasses(el, str) {
+            this._classList('remove', el, str);
+        }
+
+        _classList(action, el, str) {
+            if (typeof str === 'string') {
+                str = str.split(' ');
+            }
+            
+            for (let i = 0; i < str.length; i++) {
+				if(typeof str[i] === 'object'){
+					this._classList(action, el, str[i]);
+				}else if(str[i] !== ''){
+					el.classList[action](str[i]);
 				}
-			};
-		})();
-		
-		this.raf = window.requestAnimationFrame || 
-				  window.webkitRequestAnimationFrame ||
-				  window.msRequestAnimationFrame || raf;  
+            }
+        }
 
-		let divTest = document.createElement("div");
+        _mergeSettings(def, extra) {
+            return typeof extra !== "object" ? def : Object.assign(def, extra);
+        }
 
-		/* checking needed features */
-		this.canAnimate = (typeof this.raf === "function") &&
-					  ("classList" in divTest) &&
-					  typeof divTest.style.transition !== undefined;
+        _hide(el) {
+            el.style.display = 'none';
+        }
 
-		//change this.raf() func. context to window
-		if(this.canAnimate){
-			this.raf = this.raf.bind(window);
-		}
+        _show(el) {
+            el.style.display = 'block';
+        }
 
-		/* requestAnimationFrame queue */
-		this.frames = [];
-		this.framesRun = false;
-	}
+        _isHidden(el) {
+            return this._getStyle(el, 'display') === 'none';
+        }
 
-	show(el, options = {}){
-		//skip animate if requestAnimationFrame is not supported
-		if(!this.canAnimate){
-			this._show(el);
-		}
-		// return if element is hidden
-		if(!this._isHidden(el)){
-			return;
-		}
-
-		/* merge defaults and users options */
-		let settings = this._calcOptions(options);
-
-		/* set handler on animation finish */
-		this._setFinishHandler(el, settings.track, settings.duration, () => {
-			this._removeClasses(el, settings.classNames.enterActive);
-			this._removeClasses(el, settings.classNames.enterTo);
-			settings.afterEnter(el);
-		});
-
-		this._show(el);
-		this._addClasses(el, settings.classNames.enter);
-		settings.beforeEnter(el);
-		
-		this._addFrame(() => {
-			this._addClasses(el, settings.classNames.enterActive);
-		});
-
-		this._addFrame(() => {
-			this._removeClasses(el, settings.classNames.enter);
-			this._addClasses(el, settings.classNames.enterTo);
-		});
-	}
-
-	hide(el, options = {}){
-		if(!this.canAnimate){
-			this._hide(el);
-		}
-
-		if(this._isHidden(el)){
-			return;
-		}
-
-		let settings = this._calcOptions(options);
-
-		this._setFinishHandler(el, settings.track, settings.duration, () => {
-			this._hide(el);
-			this._removeClasses(el, settings.classNames.leaveActive);
-			this._removeClasses(el, settings.classNames.leaveTo);
-			options.systemOnEnd && options.systemOnEnd();
-			settings.afterLeave(el);
-		});
-
-		this._addClasses(el, settings.classNames.leave);
-		
-		settings.beforeLeave(el);
-		
-		this._addFrame(() => {
-			this._addClasses(el, settings.classNames.leaveActive);
-		});
-
-		this._addFrame(() => {
-			this._addClasses(el, settings.classNames.leaveTo);
-			this._removeClasses(el, settings.classNames.leave);
-		});
-	}
-
-	insert(target, el, options = {}, before = null){
-		this._hide(el);
-		target.insertBefore(el, before);
-		this.show(el, options);
-	}
-
-	remove(el, options = {}){
-		options.systemDoneCallback = function(){
-			el.parentNode.removeChild(el);
-		}
-
-		this.hide(el, options);
-	}
-
-	_setFinishHandler(el, track, duration, fn){
-		let eventName;
-		let isCssTrack = true;
-
-		if(track === 'transition'){
-			eventName = 'transitionend';
-		}
-		else if(track === 'animation'){
-			eventName = 'animationend';
-		}
-		else{
-			isCssTrack = false;
-		}
-
-		if(isCssTrack){
-			let handler = function(){
-				el.removeEventListener(eventName, handler);
-				fn();
-			};
-
-			el.addEventListener(eventName, handler);
-		}
-		else{
-			setTimeout(fn, duration);
-		}
-	}
-
-	_calcOptions(options){
-		let name = (options.name !== undefined) ? options.name : 'dl-nothing-doing-class';
-		let classNames = this._mergeSettings(this._classNames(name), options.classNames);
-
-		delete options.classNames;
-
-		let defaults = {
-			name: '',
-			track: 'transition',
-			duration: null,
-			classNames: classNames,
-			beforeEnter(el){},
-			afterEnter(el){},
-			beforeLeave(el){},
-			afterLeave(el){},
-			systemDoneCallback(el){}
-		}
-
-		let norm = this._mergeSettings(defaults, options);
-
-		/* analize track & duration error */
-
-		return norm;
-	}
-
-	_classNames(name){
-		return {
-			enter: name + '-enter',
-			enterActive: name + '-enter-active',
-			enterTo: name + '-enter-to',
-			leave: name + '-leave',
-			leaveActive: name + '-leave-active',
-			leaveTo: name + '-leave-to'
-		}
-	}
-
-	_addFrame(fn){
-		this.frames.push(fn);
-
-		if(!this.framesRun){
-			this._nextFrame();
-		}
-	}
-
-	_nextFrame(){
-		if(this.frames.length === 0){
-			this.framesRun = false;
-			return;
-		}
-
-		let frame = this.frames.shift();
-
-		this.raf(() => {
-			this.raf(() => {
-				frame();
-				this._nextFrame();
-			});
-		});
-	}
-
-	_addClasses(el, str){
-		let arr = str.split(' ');
-
-		for(let i = 0; i < arr.length; i++){
-			el.classList.add(arr[i]);
-		}
-	}
-
-	_removeClasses(el, str){
-		let arr = str.split(' ');
-
-		for(let i = 0; i < arr.length; i++){
-			el.classList.remove(arr[i]);
-		}
-	}
-
-	_mergeSettings(def, extra){
-		return typeof extra !== "object" ? def : Object.assign(def, extra);
-	}
-
-	_hide(el){
-		el.style.display = 'none';
-	}
-
-	_show(el){
-		el.style.removeProperty('display');
-		
-		if(this._isHidden(el)){
-			el.style.display = 'block';
-		}
-	}
-
-	_isHidden(el){
-		return this._getStyle(el, 'display') === 'none';
-	}
-
-	_getStyle(el, prop){
-		return getComputedStyle(el)[prop];
-	}
-}
-window.DLAnimate = new DLAnimate();
+        _getStyle(el, prop) {
+            return getComputedStyle(el)[prop];
+        }
+    }
+    window.DLAnimate = new DLAnimate();
 }();
